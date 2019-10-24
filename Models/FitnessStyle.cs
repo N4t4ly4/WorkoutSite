165baa0d31fd6ac449f9407f639c8551e1ae673a
Natalya <n4t4ly45@live.com>
2019-10-24 15:03:25 -0600

    Add files via upload

---
+++ Models/FitnessStyle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkoutSite.Models
+{
+    public class FitnessStyle
+    {
+        public virtual int FitnessStyleId { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Description { get; set; }
+        public virtual List<HealthCoach> HealthCoaches { get; set; }
+
+    }
+}
